951525449c5c6dad6f85fe1da070d6d14e6c64ba
provider161 <provider8@yandex.ru>
2019-10-27 14:08:52 +0300

    Задание №26. Работа с ОС Linux с помощью Python.
    Написать Python скрипт, который показывает:
    --1. Сетевые интерфейсы
    --2. Маршрут по умолчанию
    --3. Информацию о состоянии процессора
    --4. Информацию о процессе
    --5. Список всех процессов
    6. Статистику работы сетевых интерфейсов
    7. Статус работы какого либо сервиса
    8. Состояние сетевого порта на сервере (TCP или UDP)
    --9. Версию пакета (имя пакета передается как аргумент)
    --10. Список в файлов в директории (указать директорию)
    --11. Текущую директорию
    --12. Версию ядра
    --13. Версию операционной системы
    
    Условия:
    Пользоваться только модулями subprocess, os, system и bash внутри subprocess, так же можно читать данные из файловой системы /proc

--- utils/os_info/os_info.py
+++ utils/os_info/os_info.py
@@ -2,7 +2,11 @@
 Script prints info about host system, net ports, statistic
 """
 
-import os, sys, subprocess, argparse, json
+import os
+import sys
+import subprocess
+import argparse
+import json
 
 
 def get_args():
@@ -11,14 +15,18 @@ def get_args():
 
     args.add_argument('--package', type=str, action='store', help='Package name to show version')
     args.add_argument('--dir', type=str, action='store',  help='Directory to show files it contains', default='/')
+    args.add_argument('--port', type=str, action='store',  help="Port number to show it's type", default='22')
 
     return args.parse_args()
 
 
 def get_os_info():
+    """
+    Gather information about host system, network interfaces and statistic.
+    :return:
+        os_info - Dict with gathered data
+    """
     args = get_args()
-
-    files_in_dir = os.listdir(args.dir)
 
     try:
         package_version = subprocess.check_output(f'pip3 freeze | grep {args.package}', shell=True).decode().strip()
@@ -40,20 +48,39 @@ def get_os_info():
     except subprocess.CalledProcessError as e:
         all_processes = f'Unable to load network interfaces because of error - {e}'
 
-    os_info ={
+    try:
+        port_type = subprocess.check_output(f'lsof -nP -iTCP -sTCP:LISTEN | grep :{args.port}', shell=True).decode()
+    except subprocess.CalledProcessError as e:
+        port_type = f'Unable to load port type because of error - {e}'
+
+    try:
+        interfaces_stat = subprocess.check_output('ifconfig').decode()
+    except subprocess.CalledProcessError as e:
+        interfaces_stat = f'Unable to load interfaces statistic because of error - {e}'
+
+    try:
+        cron_status = subprocess.check_output(['systemctl', 'status', 'cron']).decode()
+    except subprocess.CalledProcessError as e:
+        cron_status = f'Unable to load cron status because of error - {e}'
+
+    os_info = {
         'current_directory': os.getcwd(),
         'os_information': os.uname(),
         'python_core': sys.version,
-        'files_list': files_in_dir,
+        'files_list': os.listdir(args.dir),
         'package_version': package_version,
         'default_path': os.getenv('PATH'),
         'network_interfaces': network_interfaces,
         'cpu_info': cpu_info,
         'process_id': os.getpid(),
-        'all_processes': all_processes
+        'all_processes': all_processes,
+        'port_type': port_type,
+        'interfaces_stat': interfaces_stat,
+        'cron_status': cron_status
     }
 
     print(json.dumps(os_info, indent=4))
 
 
-get_os_info()
+if __name__ == '__main__':
+    get_os_info()
